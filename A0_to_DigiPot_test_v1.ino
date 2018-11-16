116f9959f6b8a0a35cf916512ddd10bee05a4056
Frank Pedersen <frank@sundog.dk>
2018-11-16 02:11:01 +0100

    Add files via upload

--- A0_to_DigiPot_test_v1.ino
+++ A0_to_DigiPot_test_v1.ino
@@ -1,29 +1,33 @@
 
+
 #include <SPI.h>
+#include "SSD1306.h"
+
+#define SDA_PIN 5
+#define SCL_PIN 4
+
+SSD1306  display(0x3c, SDA_PIN, SCL_PIN);
+
 
 const int csPin = 10;
 int mapped_A0_input;
 int number = 0; // can be used in switch case of final code
 
-void setup() {
-  SPI.begin();
-  SPI.setBitOrder(MSBFIRST); // From Data Sheet
-  pinMode(csPin,OUTPUT);
-  digitalWrite(csPin, HIGH);
-  Serial.begin(9600);
-  Serial.println(number); // start somewhere
-}
 
-void loop() {
-    mapped_A0_input = map(analogRead(A0), 0, 1023, 0, 1023);
-    if (number != mapped_A0_input)
-    if (number < mapped_A0_input) number ++;
-    else number --;
-    delay(100);
-    digitalPotWrite(0,mapped_A0_input);
-    Serial.println(number);
-    delay(100);
+void setup() {
+    SPI.begin();
+    SPI.setBitOrder(MSBFIRST); // From Data Sheet
+    pinMode(csPin,OUTPUT);
+    digitalWrite(csPin, HIGH);
+    Serial.begin(115200);
+    Serial.println(number); // start somewhere
+    display.init();
+    display.flipScreenVertically();
+    display.clear();
+    display.setFont(ArialMT_Plain_24);
+    display.display();
     }
+
 
 void digitalPotWrite(int address, int value) {
     digitalWrite(csPin, LOW); //select slave
@@ -38,4 +42,22 @@ void digitalPotWrite(int address, int value) {
     }
 
 
+void oled_writenumbers() {
+    display.drawString(0, 18, "A0 in:");
+    display.drawString(66, 18, String(analogRead(A0)));
+    }
 
+
+void loop() {
+    mapped_A0_input = map(analogRead(A0), 0, 1023, 0, 1023);
+    if (number != mapped_A0_input)
+    if (number < mapped_A0_input) number ++;
+    else number --;
+    delay(50);
+    digitalPotWrite(0,mapped_A0_input);
+    Serial.println(number);
+    display.clear();
+    oled_writenumbers();
+    display.display();
+    delay(50);
+    }
